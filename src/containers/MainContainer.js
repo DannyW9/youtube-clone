c07085046503e9adb1c9e4523c4f1e739326c705
DannyW9 <dannywelsh400@hotmail.com>
2019-08-16 15:45:09 +0100

    Sets up page layout using Flexbox

--- src/containers/MainContainer.js
+++ src/containers/MainContainer.js
@@ -4,15 +4,22 @@ class MainContainer extends Component {
   render(){
     return(
       <div>
+
         <div id='nav'>
           <h1> NAVBAR </h1>
         </div>
+
         <div id='main'>
           <div id='video'>
             <h1> VIDEO </h1>
           </div>
           <div id='list'>
             <h1> VIDEO LIST </h1>
+            <h2> Video 1</h2>
+            <h2> Video 2</h2>
+            <h2> Video 3</h2>
+            <h2> Video 4</h2>
+            <h2> Video 5</h2>
           </div>
         </div>
 
